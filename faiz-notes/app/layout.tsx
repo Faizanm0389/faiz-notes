fa60025160544c5c5a408fd8ea5cdd5e324465f4
Faizanm0389 <faizanm0389@gmail.com>
2025-04-18 23:20:10 -0400

    setup

---
+++ faiz-notes/app/layout.tsx
@@ -0,0 +1,33 @@
+// app/layout.tsx
+
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/providers/ThemeProvider";
+import { Toaster } from "sonner";
+
+
+export const metadata: Metadata = {
+  title: "GOAT Notes",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster  />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
